597a2d74d36e7743658a7008c4d21284236ac2bb
whdid502 <whdid1234@naver.com>
2021-03-17 17:33:18 +0900

    week3-1 coplit_algorithm

---
+++ interactive_pre_course/week3-1_thinking_to_next_level/coplit_algorithm.js
@@ -0,0 +1,465 @@
+// function transformFirstAndLast(arr) {
+//   let resultObj = {}
+//   if(arr.length === 0){
+//     return resultObj
+//   }else if(arr.length === 1){
+//     resultObj[arr[0]] = arr[0]
+//     return resultObj 
+//   }else{
+//     resultObj[arr[0]] = arr[arr.length-1]
+//     return resultObj
+//   }
+// }
+
+// function computeWhenDouble(interestRate) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   if (interestRate >= 100) {
+//     return 1
+//   } else {
+//     let base = 100;
+//     let nextYear = base + (base * interestRate * 0.01)
+//     let i = 1;
+//     while (base * 2 >= nextYear) {
+//       nextYear = nextYear + (nextYear * interestRate * (1 / 100));
+//       i++;
+//       console.log(base, nextYear)
+//       if (base * 2 < nextYear) {
+//         console.log(i)
+//         return i + 1
+//       }
+//     }
+//   }
+// }
+
+// computeWhenDouble(2.3)
+
+// function powerOfTwo(num) {
+//   let divededByTwo = num/2
+//   let restByTwo = num%2
+//   if(num === 1){
+//     return true
+//   }else{
+//     while(restByTwo === 0){
+//       divededByTwo = divededByTwo/2
+//       restByTwo = divededByTwo%2
+//     }
+//     if (divededByTwo === 1){
+//       return true
+//     }else{
+//       return false
+//     }
+//   }
+// }
+
+// console.log(powerOfTwo(40))
+
+// function firstCharacter(str) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let strings = str.split(' ')
+//   let result = ''
+//   console.log(strings)
+//   if (str.length === 0) {
+//     return ''
+//   } else {
+//     for (let i = 0; i < strings.length; i++) {
+//       result = result + strings[i][0]
+//     }
+//     console.log(result)
+//     return result
+//   }
+// }
+
+// firstCharacter('The community at Code States might be the biggest asset')
+
+// function firstReverse(str) {
+//   let strings = str.split('')
+//   console.log(strings)
+//   let reverseStrings = strings.reverse()
+//   console.log(reverseStrings)
+//   let result = String(reverseStrings.join(''))
+//   console.log(result)
+//   return result
+// }
+
+// firstReverse('I love codestates')
+
+// function powerOfTwo(num) {
+//   let divededByTwo = num/2
+//   let restByTwo = num%2
+//   if(num === 1){
+//     return true
+//   }else{
+//     while(restByTwo === 0){
+//       divededByTwo = divededByTwo/2
+//       restByTwo = divededByTwo%2
+//     }
+//     if (divededByTwo === 1){
+//       return true
+//     }else{
+//       return false
+//     }
+//   }
+// }
+
+// console.log(powerOfTwo(16))
+// console.log(0/2)
+
+// function letterCapitalize(str) {
+//   // TODO: 여기에 코드를 작성합니다.
+
+//   if (str.length === 0) {
+//     return ''
+//   } else {
+//     let strings = str.split('')
+//     for (let i = 1; i < strings.length; i++) {
+//       if (strings[i - 1] === ' ') {
+//         let upperStr = strings[i].toUpperCase()
+//         strings[i] = upperStr
+//       }
+//     }
+//     const firstWord = strings[0].toUpperCase()
+//     strings[0] = firstWord
+//     // console.log(strings.join(''))
+//     return strings.join('')
+//   }
+// }
+
+// letterCapitalize('javascript  is sexy ');
+
+// function convertListToObject(arr) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let resultObj = {}
+//   if (arr.length === 0) {
+//     return resultObj
+//   } else {
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i].length !== 0) {
+//         if (!(Object.keys(resultObj).includes(arr[i][0]))) {
+//           resultObj[arr[i][0]] = arr[i][1]
+//         }
+
+//       }
+//       console.log(Object.keys(resultObj))
+//     }
+//   }
+//   console.log(resultObj)
+//   return resultObj
+// }
+
+// const arr = [['some', 'like'], ['first', 'second'], ['mike', 'ike'], ['first', 'Fox'], ['melee', 'brawl']]
+
+// convertListToObject(arr)
+
+// function convertDoubleSpaceToSingle(str) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let strings = str.split(' ')
+//   // console.log(strings)
+//   let newStrings = []
+//   for (let i = 0 ; i < strings.length ; i++){
+//     if(strings[i].length !== 0){
+//       newStrings.push(strings[i])
+//     }
+//   }
+//   // console.log(newStrings)
+//   let result = newStrings.join(' ')
+//   // console.log(result)
+//   return result
+// }
+
+// convertDoubleSpaceToSingle('string  with  double  spaces');
+
+// function ABCheck(str) {
+//   // code goes here
+//   let lowerStr = str.toLowerCase()
+//   let strArray = lowerStr.split('')
+//   let aIndex = []
+//   let bIndex = []
+//   let distances = []
+//   for (let i = 0; i < strArray.length ; i++){
+//     if(strArray[i] === 'a'){
+//       aIndex.push(i)
+//     }else if(strArray[i] === 'b'){
+//       bIndex.push(i)
+//     }
+//   }
+//   for (let a = 0 ; a < aIndex.length ; a++){
+//     for (let b = 0 ; b < bIndex.length ; b++){
+//       let distance = Math.abs(aIndex[a] - bIndex[b])-1
+//       distances.push(distance)
+//     }
+//   }
+//   if(distances.includes(5)){
+//     return true
+//   }else{
+//     return flase
+//   }
+// }
+
+// ABCheck('lane Badesfeborraoweaddddmmm mdbddmdda');
+
+
+
+// let newArr = arr.substr(0,2).concat('-').concat(arr.substr(2,4))
+// console.log(newArr)
+
+// function insertDash(str) {
+//   for(let i = 1 ;  i < str.length ; i++){
+//    if(Number(str[i-1])%2 === 1 && Number(str[i])%2 === 1){
+//       str = str.substr(0,i).concat('-').concat(str.substr(i,str.length-1))
+//     }    
+//   }
+//   return str
+// }
+
+// insertDash('454793');
+
+// function insertDash(str) {
+//   str = str.split('');
+//   let front;
+//   let back;
+//   let newArray
+
+//   for (let i = 1; i < str.length; i++) {
+//     if ((Number(str[i - 1]) % 2 === 1) && (Number(str[i]) % 2 === 1)) {
+
+//       front = str.slice(0, str[i])
+//       back = str.slice(str[i], str.length-1)
+//       str = front.concat('-').concat(back)
+//       // console.log(i)
+//     }
+//   }
+//   // console.log(str)
+//   str = str.join('');
+//   return str;
+// }
+
+// insertDash('6734611997679419173')
+
+// function removeExtremes(arr) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let arrLength = []
+//   for (let i = 0; i < arr.length; i++) {
+//     arrLength.push(arr[i].length)
+//   }
+//   let maxLen = Math.max.apply(null, arrLength)
+//   let minLen = Math.min.apply(null, arrLength)
+//   let maxCount = 0
+//   let minCount = 0
+
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     if (maxCount === 0 && arr[i].length === maxLen) {
+//       maxCount += 1
+//       arr.splice(i, 1)
+//     }
+//   }
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     if (minCount === 0 && arr[i].length === minLen) {
+//       minCount += 1
+//       arr.splice(i, 1)
+//     }
+//   }
+//   console.log(arr)
+
+
+//   return arr
+// }
+
+// // removeExtremes(['iAmLongest', 'iAmNotShortest', 'iAmLongest', 'long'])
+// removeExtremes(['a', 'c', 'def'])
+
+// function findBugInApples(arr) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let bIndex = []
+//   for(let i = 0 ; i < arr.length ; i++){
+//     for( let n = 0 ; n < arr[i].length; n++){
+//       if(arr[i][n] === 'B'){
+//         bIndex.push(i)
+//         bIndex.push(n)
+//       }
+//     }
+//   }
+//   console.log(bIndex)
+//   return bIndex
+// }
+
+// findBugInApples([
+//   ['A', 'A', 'A', 'A', 'A'],
+//   ['A', 'B', 'A', 'A', 'A'],
+//   ['A', 'A', 'A', 'A', 'A'],
+//   ['A', 'A', 'A', 'A', 'A'],
+//   ['A', 'A', 'A', 'A', 'A'],
+// ]);
+
+
+// let test = 'ddd'
+
+// console.log(test[3])
+
+// function readVertically(arr) {
+//   // code goes here
+//   let vertList = []
+//   let arrLenList = arr.map((el) => el.length)
+//   let maxLen = Math.max(...arrLenList)
+//   // console.log(maxLen)
+//   for (let i = 0; i < maxLen; i++) {
+//     for (let n = 0; n < arr.length; n++) {
+//       // console.log(arr[n][i])
+//       if (arr[n][i] === undefined) {
+//         vertList.push('')
+//       } else {
+//         vertList.push(arr[n][i])
+//       }
+//     }
+//   }
+//   // console.log(vertList)
+//   return vertList.join('')
+// }
+
+// input = ['YCDt',
+//   'oao',
+//   'unI']
+
+// console.log(readVertically(input))
+
+// function superIncreasing(arr) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let preArr
+//   let preSum
+//   for(let i = 1 ; i < arr.length ; i++){
+//     preArr = arr.slice(0,i)
+//     preSum = preArr.reduce((acc,cur) => acc+cur)2
+//     console.log(preArr)
+//     console.log(arr[i])
+//     if(arr[i] <= preSum){
+//       return false
+//     }
+//   }
+//   return true
+// }
+
+// console.log(superIncreasing( [2456, -826, 32, 553, -1346, 376, 1969, -47]))
+
+// console.log(superIncreasing( [612, 1226]))
+
+// function modulo(num1, num2) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let remainder = num1 - num2
+
+//   if (num2 === 0) {
+//     return 'Error: cannot divide by zero'
+//   } else {
+
+//     if (num1 > num2) {
+//       while (remainder >= num2) {
+//         remainder = remainder - num2
+//       }
+//       return remainder
+//     } else if (num1 === num2) {
+//       return 0
+//     } else {
+//       return num1
+//     }
+//   }
+//   // console.log(remainder)
+
+// }
+
+// console.log(modulo(99, 4))
+
+// function isIsogram(str) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let count = 0
+//   if (str.length === 0) {
+//     return true
+//   } else {
+//     let lowerStr = str.toLowerCase()
+//     for (let i = 0; i < lowerStr.length; i++) {
+//       for (let n = 0; n < lowerStr.length; n++) {
+//         // console.log(lowerStr[i],lowerStr[n])
+//         if (lowerStr[i] === lowerStr[n]) {
+//           count += 1
+//         }
+//       }
+//     }
+//     console.log(count)
+//     if (count > str.length) {
+//       return false
+//     } else {
+//       return true
+//     }
+//   }
+// }
+
+// console.log(isIsogram('iCanDo'))
+
+// function computeSquareRoot(num) {
+//   let approx = 1;
+
+//   while (approx ** 2 !== num) {
+//     if ( Number((approx*approx).toFixed(2)) === num) {
+//       break;
+//     }
+//     approx = (approx + (num / approx)) / 2;
+//   } 
+
+//   return Number(approx.toFixed(2));
+// }
+
+
+// console.log(isNaN(Number(' ')))
+// console.log(Number('1'))
+
+// function numberSearch(str) {
+//   // TODO: 여기에 코드를 작성합니다.
+//   let strLen = 0
+//   let sum = 0
+//   if (str.length === 0) {
+//     return 0
+//   } else {
+
+//     for (let i = 0; i < str.length; i++) {
+//       if (!isNaN(Number(str[i]))) {
+//         console.log(str[i], sum)
+//         // console.log(sum)
+//         sum = sum + Number(str[i])
+//       }else{
+//         strLen += 1
+//       }
+//     }
+//     console.log(strLen, sum)
+//     console.log(sum/strLen)
+//     return Math.round(sum / strLen)
+//   }
+// }
+
+// // console.log(numberSearch("Sou2bgPJkS7Lp5r2j6jeWOts8X"))
+// console.log(numberSearch('8ad'))
+
+function decryptCaesarCipher(str, secret) {
+  // TODO: 여기에 코드를 작성합니다.
+  if (str.length === 0) {
+    return ''
+  } else {
+    let alph = 'abcedfghijklmnopqrstuvwxyz'
+    let lStr = str.toLowerCase()
+    let result = ''
+    for (let i = 0; i < str.length; i++) {
+      for (let n = 0; n < alph.length; n++) {
+        if (lStr[i] === alph[n]) {
+          if (n < secret) {
+            let index = alph.length - Math.abs(n - secret) + 1
+            result += alph[index]
+          }
+          if (n >= secret) {
+            result += alph[n - secret]
+            
+          }
+          console.log(lStr[i], alph[n], secret, result)
+        }
+      }
+    }
+    console.log(result)
+  }
+}
+
+decryptCaesarCipher('nzop delepd dfaazced jzf', 11)
