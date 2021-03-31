aad09cce9aabf25f278fafdb33e6ee0beed5fc11
whdid502 <whdid1234@naver.com>
2021-03-31 11:22:17 +0900

    scope private study

---
+++ interactive_pre_course/week4-2_recursion/closure&scope.js
@@ -0,0 +1,45 @@
+// let brave_girls = 'rollin'
+// function k_pop(){
+//  let brave_girls = 'just dirve'
+//  console.log(brave_girls)
+// }
+// k_pop()
+// console.log(brave_girls)
+
+// let chicken = 'nene'
+
+// {
+//   let chicken = 'kychon'
+//   console.log(chicken)
+// }
+// console.log(chicken)
+
+// function pizza(){
+//   domino = 'is good'
+// }
+
+// pizza()
+
+// console.log(domino)
+
+// let dismas = 'live'
+
+// function condition_dismas(){
+//   console.log(dismas)
+// }
+
+// function kill_dismas(){
+//   dismas = 'die'
+//   condition_dismas();
+// }
+// kill_dismas()
+
+let dismas = 'live'
+function condition_dismas(){
+ console.log(dismas)
+}
+function kill_dismas(){
+ let dismas = 'die'
+ condition_dismas();
+}
+kill_dismas() // 'die'
