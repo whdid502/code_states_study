b3b471214183c73fe68da860b1233fa58307966c
whdid502 <whdid1234@naver.com>
2021-04-06 09:04:11 +0900

    sprit0 04_nano

---
+++ immersive_course/coplit_toy_problem_test.js
@@ -0,0 +1,14 @@
+const rockPaperScissors = function (rounds) {
+  // TODO: 여기에 코드를 작성합니다.
+  const rpc_arr = ['rock','paper','sissors'] 
+  let result = [];
+  for(let i  = 0 ; i < rounds ; i ++){
+    
+  }
+
+  return result
+};
+
+let output = rockPaperScissors(5)
+
+console.log(output)
