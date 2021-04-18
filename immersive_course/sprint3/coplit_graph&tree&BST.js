5d617127e3ed875c2614e17c9050e1d1bf74e03a
whdid502 <whdid1234@naver.com>
2021-04-18 10:57:08 +0900

    dfs study

--- immersive_course/sprint3/coplit_graph&tree&BST.js
+++ immersive_course/sprint3/coplit_graph&tree&BST.js
@@ -350,29 +350,114 @@
 // ]);
 // console.log(result4)
 \
-function barcode(len) {
-  // TODO: 여기에 코드를 작성하세요.
+// function barcode(len) {
+//   // TODO: 여기에 코드를 작성하세요.
 
-  let answer
-  const aux = (size, num_str) => {
-    if(size === len){
-      if(is_valid(num_str) === true){
-        answer = num_str
-        return true
-      }else{
-        return false
-      }
-    }
-  if(aux(size + 1, num_str + '1') === true) return true
-  if(aux(size + 1, num_str + '2') === true) return true
-  if(aux(size + 1, num_str + '3') === true) return true
+//   let answer
+//   const aux = (size, num_str) => {
+//     if(size === len){
+//       if(is_valid(num_str) === true){
+//         answer = num_str
+//         return true
+//       }else{
+//         return false
+//       }
+//     }
+//   if(aux(size + 1, num_str + '1') === true) return true
+//   if(aux(size + 1, num_str + '2') === true) return true
+//   if(aux(size + 1, num_str + '3') === true) return true
 
-  }
+//   }
 
-  aux(0,'1')
-}
+//   aux(0,'1')
+// }
 
 
-//내가 원하는 길이만큼의 숫자수열을 만들 수 있는가부터
+// //내가 원하는 길이만큼의 숫자수열을 만들 수 있는가부터
 
-//만들어진 숫자 수열로 조건에 만족하는지
+// //만들어진 숫자 수열로 조건에 만족하는지
+
+// class Node {
+//   constructor(value){
+//     this.left = null;
+//     this.right = null;
+//     this.value = value;
+//   }
+// }
+
+// class BinarySearchTree {
+//   constructor(){
+//     this.root = null;
+//   }
+//   DFTPreOrder(currentNode, list) {
+//     return traversePreOrder(this.root, []);
+//   }
+//   DFTPostOrder(){
+//     return traversePostOrder(this.root, []); 
+//   }
+//   DFTInOrder(){
+//     return traverseInOrder(this.root, []);
+//   } 
+// }
+
+// function traversePreOrder(node, list){
+//   list.push(node.value);
+//   if(node.left) {
+//     traversePreOrder(node.left, list);
+//   }
+//   if(node.right) {
+//     traversePreOrder(node.right, list);
+//   }
+//   return list;
+// }
+
+// function traverseInOrder(node, list){
+//   if(node.left) {
+//     traverseInOrder(node.left, list);
+//   }
+//   list.push(node.value);
+//   if(node.right) {
+//     traverseInOrder(node.right, list);
+//   }
+//   return list;
+// }
+
+// function traversePostOrder(node, list){
+//   if(node.left) {
+//     traversePostOrder(node.left, list);
+//   }
+//   if(node.right) {
+//     traversePostOrder(node.right, list);
+//   }
+//   list.push(node.value);
+//   return list;
+// }
+
+
+// const tree = new BinarySearchTree();
+// tree.insert(9)
+// tree.insert(4)
+// tree.insert(6)
+// tree.insert(20)
+// tree.insert(170)
+// tree.insert(15)
+// tree.insert(1)
+// // tree.remove(170);
+// // JSON.stringify(traverse(tree.root))
+
+// console.log('BFS', tree.BreadthFirstSearch());
+// console.log('BFS', tree.BreadthFirstSearchR([tree.root], []))
+// console.log('DFSpre', tree.DFTPreOrder());
+// console.log('DFSin', tree.DFTInOrder());
+// console.log('DFSpost', tree.DFTPostOrder());
+
+// //     9
+// //  4     20
+// //1  6  15  170
+
+// function traverse(node) {
+//   const tree = { value: node.value };
+//   tree.left = node.left === null ? null : traverse(node.left);
+//   tree.right = node.right === null ? null : traverse(node.right);
+//   return tree;
+// }
