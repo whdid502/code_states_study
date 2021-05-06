923d5b2f3e623650646a8ad4ebf401d083e20bab
whdid502 <whdid1234@naver.com>
2021-05-06 10:16:28 +0900

    toy 09 power

---
+++ immersive_course/toy_algorithm/09_power.js
@@ -0,0 +1,19 @@
+function power(base, exponent) {
+  // todo: 여기에 코드를 작성합니다.
+  const divedNum = 94906249
+  if(exponent === 0){
+    return 1
+  }
+  else if(exponent === 1){
+    return base
+  }
+  else if(exponent%2 === 0){
+
+  }
+  else{
+
+  }
+}
+
+let output = power(3, 40);
+console.log(output); // --> 19334827
