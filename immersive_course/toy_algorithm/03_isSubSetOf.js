853c8d0f0035626471f9332703c4182b17505cb6
whdid502 <whdid1234@naver.com>
2021-05-19 10:28:56 +0900

    toy 03 issubsetof add

--- immersive_course/toy_algorithm/03_isSubSetOf.js
+++ immersive_course/toy_algorithm/03_isSubSetOf.js
@@ -72,3 +72,23 @@ const isSubsetOf = function (base, sample) {
   }
   return true;
 };
+
+const isSubsetOf = function (base, sample) {
+  base.sort((a, b) => a - b);
+  sample.sort((a, b) => a - b);
+
+  const findItemInSortedArr = (item, arr, from) => {
+    for (let i = from; i < arr.length; i++) {
+      if (item === arr[i]) return i;
+      else if (item < arr[i]) return -1;
+    }
+    return -1;
+  };
+
+  let baseIdx = 0;
+  for (let i = 0; i < sample.length; i++) {
+    baseIdx = findItemInSortedArr(sample[i], base, baseIdx);
+    if (baseIdx === -1) return false;
+  }
+  return true;
+};
