341cc0742b9be44cd4088ab4ff6529cd64715737
whdid502 <whdid1234@naver.com>
2021-05-20 09:33:18 +0900

    toy 16 quick sort

---
+++ immersive_course/toy_algorithm/16_quick_sort.js
@@ -0,0 +1,16 @@
+const quickSort = function (arr) {
+  // TODO: 여기에 코드를 작성합니다.
+  if (arr.length < 2)
+    return arr;
+
+  let pivot = arr[Math.floor(arr.length / 2)];
+  let middle = arr.filter(function (data) { return data == pivot; });
+  let lows = quickSort(arr.filter(function (data) { return data < pivot; }));
+  let highs = quickSort(arr.filter(function (data) { return data > pivot; }));
+
+  return lows.concat(middle).concat(highs);
+};
+
+
+let output = quickSort([20, -10, -11, 2, 29]);
+console.log(output); // --> [1, 3, 21]
