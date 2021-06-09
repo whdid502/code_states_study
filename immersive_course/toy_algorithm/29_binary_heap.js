a492322c663fc75ab6f4e5b9368d57d6aca0ebbd
whdid502 <whdid1234@naver.com>
2021-06-09 09:52:55 +0900

    toy 29 bynary heap

---
+++ immersive_course/toy_algorithm/29_binary_heap.js
@@ -0,0 +1,41 @@
+// 아래 코드는 수정하지 마세요.
+function swap(idx1, idx2, arr) {
+  // 두 변수를 바꾸는 방법
+
+  // 1) 임시 변수를 활용한 방법
+  // let temp = arr[idx1];
+  // arr[idx1] = arr[idx2];
+  // arr[idx2] = temp;
+
+  // 2) Destructuring assignment를 활용한 방법
+  // arr이 reference type이라 가능
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+
+  // 3) XOR 연산을 활용한 방법
+  // arr이 reference type이라 가능
+  // arr[idx1] ^= arr[idx2];
+  // arr[idx2] ^= arr[idx1];
+  // arr[idx1] ^= arr[idx2];
+}
+
+function getParentIdx(idx) {
+  return Math.floor((idx - 1) / 2);
+};
+
+function insert(heap, item) {
+  heap.push(item);
+  let curIdx = heap.length - 1;
+  let pIdx = getParentIdx(curIdx);
+  while (pIdx >= 0 && heap[curIdx] > heap[pIdx]) {
+    swap(curIdx, pIdx, heap);
+    curIdx = pIdx;
+    pIdx = getParentIdx(curIdx);
+  }
+  return heap;
+};
+// 아래 코드는 수정하지 마세요.
+const binaryHeap = function (arr) {
+  return arr.reduce((heap, item) => {
+    return insert(heap, item);
+  }, []);
+};
